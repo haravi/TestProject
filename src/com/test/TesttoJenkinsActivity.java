646cc4cfd18435e9f836fcd7bdf269bf2f3769ee
shivakumar haravi <shivakumar.haravi@frogdesign.com>
2012-08-17 16:56:14 +0530

    Test after modifying

--- src/com/test/TesttoJenkinsActivity.java
+++ src/com/test/TesttoJenkinsActivity.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class TesttoJenkinsActivity extends Activity {
-	// Shiva: for test purpose
-
-	// Shivakumar: For testing purpose
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
